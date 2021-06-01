d88b61438b89c3d1de2d3817eee162d1a23f4470
mrxuxg <mrxuxg@gmail.com>
2021-06-01 14:04:46 +0800

    fix Util

--- include/Util.h
+++ include/Util.h
@@ -2,11 +2,16 @@
 #include <cstdlib>
 #include <string>
 
+
+// Read "n" bytes from a descriptor.
 ssize_t readn(int fd, void *buff, size_t n);
 ssize_t readn(int fd, std::string &inBuffer, bool &zero);
 ssize_t readn(int fd, std::string &inBuffer);
+
+// Write "n" bytes to a descriptor.
 ssize_t writen(int fd, void *buff, size_t n);
 ssize_t writen(int fd, std::string &sbuff);
+
 void handle_for_sigpipe();
 int setSocketNonBlocking(int fd);
 void setSocketNodelay(int fd);
