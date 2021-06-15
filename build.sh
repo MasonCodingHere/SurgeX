25c7ffac1beebeb02ea608287e75fad56468a42f
mrxuxg <mrxuxg@gmail.com>
2021-06-15 13:12:47 +0800

    new SurgeX

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x
-
-SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-./build}
-BINARY_DIR=${BINARY_DIR:-./bin}
-mkdir $BUILD_DIR $BINARY_DIR \
-    && cd $BUILD_DIR \
-    && cmake $SOURCE_DIR \
-    && make $*
+make server
