ca7f925c5d819f4aa8ed66f599c0cfea6bfb6dae
mrxuxg <mrxuxg@gmail.com>
2021-06-15 13:58:20 +0800

    fix CMakeLists.txt

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-make server
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-./build}
+BINARY_DIR=${BINARY_DIR:-./bin}
+mkdir $BUILD_DIR $BINARY_DIR \
+    && cd $BUILD_DIR \
+    && cmake $SOURCE_DIR \
+    && make $*
