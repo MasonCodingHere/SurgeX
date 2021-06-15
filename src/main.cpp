6ffb34b9957c698129603b05cecf4d7fc86ad80d
mrxuxg <mrxuxg@gmail.com>
2021-06-15 15:54:58 +0800

    travis

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     //需要修改的数据库信息,登录名,密码,库名
     string user = "root";
-    string passwd = "Xxgsdumech2019.";
+    string passwd = "";
     string databasename = "SurgeX_DB";
 
     //命令行解析
