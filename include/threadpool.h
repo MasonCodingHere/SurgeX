4b0f5f51936cb377503dfe02ea903c7bf4276e2e
mrxuxg <mrxuxg@gmail.com>
2021-07-06 13:07:40 +0800

    small fix

--- include/threadpool.h
+++ include/threadpool.h
@@ -9,10 +9,9 @@
 #include "sql_connection_pool.h"
 
 template <typename T>
-class threadpool
-{
+class threadpool{
 public:
-    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    //thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量
     threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
     ~threadpool();
     bool append(T *request, int state);
@@ -33,39 +32,40 @@ private:
     connection_pool *m_connPool;  //数据库
     int m_actor_model;          //模型切换
 };
+
 template <typename T>
-threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
-{
+threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests)
+    : m_actor_model(actor_model),
+      m_thread_number(thread_number), 
+      m_max_requests(max_requests), 
+      m_threads(NULL),
+      m_connPool(connPool){
     if (thread_number <= 0 || max_requests <= 0)
         throw std::exception();
     m_threads = new pthread_t[m_thread_number];
     if (!m_threads)
         throw std::exception();
-    for (int i = 0; i < thread_number; ++i)
-    {
-        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
-        {
+    for (int i = 0; i < thread_number; ++i){
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0){
             delete[] m_threads;
             throw std::exception();
         }
-        if (pthread_detach(m_threads[i]))
-        {
+        if (pthread_detach(m_threads[i])){
             delete[] m_threads;
             throw std::exception();
         }
     }
 }
+
 template <typename T>
-threadpool<T>::~threadpool()
-{
+threadpool<T>::~threadpool(){
     delete[] m_threads;
 }
+
 template <typename T>
-bool threadpool<T>::append(T *request, int state)
-{
+bool threadpool<T>::append(T *request, int state){
     m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
+    if (m_workqueue.size() >= m_max_requests){
         m_queuelocker.unlock();
         return false;
     }
@@ -75,12 +75,11 @@ bool threadpool<T>::append(T *request, int state)
     m_queuestat.post();
     return true;
 }
+
 template <typename T>
-bool threadpool<T>::append_p(T *request)
-{
+bool threadpool<T>::append_p(T *request){
     m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
+    if (m_workqueue.size() >= m_max_requests){
         m_queuelocker.unlock();
         return false;
     }
@@ -89,22 +88,20 @@ bool threadpool<T>::append_p(T *request)
     m_queuestat.post();
     return true;
 }
+
 template <typename T>
-void *threadpool<T>::worker(void *arg)
-{
+void *threadpool<T>::worker(void *arg){
     threadpool *pool = (threadpool *)arg;
     pool->run();
     return pool;
 }
+
 template <typename T>
-void threadpool<T>::run()
-{
-    while (true)
-    {
+void threadpool<T>::run(){
+    while (true){
         m_queuestat.wait();
         m_queuelocker.lock();
-        if (m_workqueue.empty())
-        {
+        if (m_workqueue.empty()){
             m_queuelocker.unlock();
             continue;
         }
@@ -113,37 +110,29 @@ void threadpool<T>::run()
         m_queuelocker.unlock();
         if (!request)
             continue;
-        if (1 == m_actor_model)
-        {
-            if (0 == request->m_state)
-            {
-                if (request->read_once())
-                {
+        if (1 == m_actor_model){
+            if (0 == request->m_state){
+                if (request->read_once()){
                     request->improv = 1;
                     connectionRAII mysqlcon(&request->mysql, m_connPool);
                     request->process();
                 }
-                else
-                {
+                else{
                     request->improv = 1;
                     request->timer_flag = 1;
                 }
             }
-            else
-            {
-                if (request->write())
-                {
+            else{
+                if (request->write()){
                     request->improv = 1;
                 }
-                else
-                {
+                else{
                     request->improv = 1;
                     request->timer_flag = 1;
                 }
             }
         }
-        else
-        {
+        else{
             connectionRAII mysqlcon(&request->mysql, m_connPool);
             request->process();
         }
